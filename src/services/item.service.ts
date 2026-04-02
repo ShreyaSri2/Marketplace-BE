25fe0dba97ffe982dcd0fc6d49d0d173af03d470
Shreya <shreyasrivastav2@deloitte.com>
2026-04-02 13:52:19 +0530

    New Commit - Filter, Search, Pagination, Sorting Fix

--- src/services/item.service.ts
+++ src/services/item.service.ts
@@ -12,33 +12,45 @@ export const getAllItems = async (query: any, userId: string) => {
     order = "desc",
   } = query;
 
+  const pageNumber = Math.max(1, Number(page));
+  const limitNumber = Math.max(1, Number(limit));
+
   const filter: any = {
-    seller: { $ne: userId }, 
+    createdBy: { $ne: userId }, 
+    isSold: false,
   };
 
   if (search) {
-    filter.name = { $regex: search, $options: "i" };
+    //filter.name = { $regex: search, $options: "i" };
+    filter.$or = [
+      {name: { $regex: search, $options: "i"}},
+      {description: { $regex: search, $options: "i"}},
+    ];
   }
 
   if (category) filter.category = category;
+
   if (minPrice || maxPrice) {
     filter.price = {};
     if (minPrice) filter.price.$gte = Number(minPrice);
     if (maxPrice) filter.price.$lte = Number(maxPrice);
   }
 
-  const skip = (Number(page) - 1) * Number(limit);
+  const skip = (pageNumber - 1) * limitNumber;
+
+  const allowedSortFields = ["price", "createdAt", "name"];
+  const sortField = allowedSortFields.includes(sortBy) ? sortBy : "createdAt";
 
   const sortOptions: any = {
-    [sortBy]: order === "asc" ? 1 : -1,
+    [sortField]: order === "asc" ? 1 : -1,
   };
 
   const [items, total] = await Promise.all([
     Item.find(filter)
-      .populate("seller", "name email")
+      .populate("createdBy", "name email")
       .sort(sortOptions)
       .skip(skip)
-      .limit(Number(limit)),
+      .limit(limitNumber),
 
     Item.countDocuments(filter),
   ]);
@@ -47,9 +59,9 @@ export const getAllItems = async (query: any, userId: string) => {
     items,
     pagination: {
       total,
-      page: Number(page),
-      limit: Number(limit),
-      totalPages: Math.ceil(total / limit),
+      page: pageNumber,
+      limit: limitNumber,
+      totalPages: Math.ceil(total / limitNumber),
     },
   };
 };
